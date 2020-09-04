a5d581fa08c3296661489521092e1e8c3dbd8fe5
Lucas <lukas-gabriel2002@hotmail.com>
2020-09-04 19:48:34 -0300

    adiciona rotas

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const transactionRouter = express.Router();
 
+const {
+  getPeriod,
+  postTransaction,
+  putTransaction,
+  deleteTransaction,
+} = require('../services/transactionService');
+
+transactionRouter
+  .route('/')
+  .get(getPeriod)
+  .post(postTransaction)
+  .put(putTransaction)
+  .delete(deleteTransaction);
+
 module.exports = transactionRouter;
