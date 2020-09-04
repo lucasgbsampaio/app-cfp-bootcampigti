49cc83d3c90d2f34aace07e8d1f137d6b1d1d069
Lucas <lukas-gabriel2002@hotmail.com>
2020-09-04 19:47:54 -0300

    adiciona rotas

--- services/transactionService.js
+++ services/transactionService.js
@@ -6,3 +6,30 @@ const ObjectId = mongoose.Types.ObjectId;
 // o servidor é Linux, isso faz diferença. Gastei umas boas horas tentando
 // descobrir esse erro :-/
 const TransactionModel = require('../models/TransactionModel');
+
+const getPeriod = async (req, res) => {
+  try {
+  } catch {}
+};
+
+const postTransaction = async (req, res) => {
+  try {
+  } catch {}
+};
+
+const putTransaction = async (req, res) => {
+  try {
+  } catch {}
+};
+
+const deleteTransaction = async (req, res) => {
+  try {
+  } catch {}
+};
+
+module.exports = {
+  getPeriod,
+  postTransaction,
+  putTransaction,
+  deleteTransaction,
+};
