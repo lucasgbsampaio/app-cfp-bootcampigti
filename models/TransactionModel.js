04c5669380e53c3f0690b925647cc6f408e5e2a2
Lucas <lukas-gabriel2002@hotmail.com>
2020-09-04 20:30:47 -0300

    desativando versionKey

--- models/TransactionModel.js
+++ models/TransactionModel.js
@@ -1,43 +1,48 @@
 const mongoose = require('mongoose');
 
-const schema = mongoose.Schema({
-  description: {
-    type: String,
-    required: true,
+const schema = mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+    month: {
+      type: Number,
+      required: true,
+    },
+    day: {
+      type: Number,
+      required: true,
+    },
+    yearMonth: {
+      type: String,
+      required: true,
+    },
+    yearMonthDay: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
   },
-  value: {
-    type: Number,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  year: {
-    type: Number,
-    required: true,
-  },
-  month: {
-    type: Number,
-    required: true,
-  },
-  day: {
-    type: Number,
-    required: true,
-  },
-  yearMonth: {
-    type: String,
-    required: true,
-  },
-  yearMonthDay: {
-    type: String,
-    required: true,
-  },
-  type: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    versionKey: false,
+  }
+);
 const TransactionModel = mongoose.model('transaction', schema);
 
 module.exports = TransactionModel;
