1e9b159c40d0dfab76d75197f4753bb1df52a46c
Lucas <lukas-gabriel2002@hotmail.com>
2020-09-04 22:20:38 -0300

    persistencia de dados finalizado

--- services/transactionService.js
+++ services/transactionService.js
@@ -1,6 +1,3 @@
-const mongoose = require('mongoose');
-const ObjectId = mongoose.Types.ObjectId;
-
 // Aqui havia um erro difícil de pegar. Importei como "transactionModel",
 // com "t" minúsculo. No Windows, isso não faz diferença. Mas como no Heroku
 // o servidor é Linux, isso faz diferença. Gastei umas boas horas tentando
@@ -9,22 +6,80 @@ const TransactionModel = require('../models/TransactionModel');
 
 const getPeriod = async (req, res) => {
   try {
-  } catch {}
+    const { period } = req.query;
+
+    if (!period) {
+      return res.status(404).send({
+        error: 'Informe o "period" no formato yyyy-mm',
+      });
+    }
+
+    const transactions = await TransactionModel.find({ yearMonth: period });
+
+    return res.status(200).send(transactions);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
 
 const postTransaction = async (req, res) => {
   try {
-  } catch {}
+    const transaction = req.body;
+
+    if (!transaction) {
+      return res.status(404).send({
+        error: 'Informe todos os campos de uma transação',
+      });
+    }
+
+    const newTransaction = new TransactionModel(transaction);
+    await newTransaction.save();
+
+    return res.status(200).send(newTransaction);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
 
 const putTransaction = async (req, res) => {
   try {
-  } catch {}
+    const { _id } = req.query;
+    const transaction = req.body;
+
+    if (!_id || !transaction) {
+      return res.status(404).send({
+        error: 'Informe o _id via query e a transação pelo body',
+      });
+    }
+
+    const updateTransaction = await TransactionModel.findByIdAndUpdate(
+      _id,
+      transaction,
+      { new: true }
+    );
+
+    return res.status(200).send(updateTransaction);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
 
 const deleteTransaction = async (req, res) => {
   try {
-  } catch {}
+    const { _id } = req.query;
+
+    if (!_id) {
+      return res.status(404).send({
+        error: 'Informe o _id',
+      });
+    }
+
+    await TransactionModel.deleteOne({ _id });
+
+    return res.status(200).json({ message: 'Transação deletada com sucesso' });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
 
 module.exports = {
